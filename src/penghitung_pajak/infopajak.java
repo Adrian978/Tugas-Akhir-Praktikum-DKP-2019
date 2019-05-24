bc26cd27518034822ac8fccf1602c68a2a31b6de
Adrian978 <adriansyah978@gmail.com>
2019-05-24 20:21:16 +0700

    first commit

---
+++ src/penghitung_pajak/infopajak.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package penghitung_pajak;
+
+/**
+ *
+ * @author ADITYA CP
+ */
+public class infopajak {
+    //deklarasi atribut
+    private String info;
+    
+    //membuat setter
+    public void setInfo(String info) {
+        this.info = info;
+    }
+    
+    //membuat getter
+    public String getInfo(){
+        return info;
+    }
+}
